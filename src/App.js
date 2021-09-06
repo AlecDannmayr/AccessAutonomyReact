6927b9498447ca98dfac6bee99db5692cf2d1443
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-06 12:26:16 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ function App() {
    return (
       <div className="App">
          <header className="App-header">
-            <p>
-               Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-               Learn React
-            </a>
+            <svg width="200" height="250">
+               <path d="M14.83,20.82h0a1.28,1.28,0,0,1-1-.52L.25,2a1.27,1.27,0,0,1,2-1.51L14.84,17.45,27.73.5a1.26,1.26,0,0,1,2,1.53L15.84,20.32A1.28,1.28,0,0,1,14.83,20.82Z"></path>
+            </svg>
          </header>
       </div>
    )
---
+++ src/svgs/hexagon.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function hexagon() {
+   return (
+      <svg width="200" height="250">
+         <polygon stroke-linejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon>
+      </svg>
+   )
+}
+export default hexagon

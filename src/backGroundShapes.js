40a4949d700934c23133b78ff8247441484f6bf2
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-06 14:57:20 +0100

    --- Auto-Git Commit ---

--- src/backGroundShapes.js
+++ src/backGroundShapes.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import Circle from './svgs/circle'
 import Square from './svgs/square'
-import ArrowUp from './svgs/arrowUp'
-import ArrowDown from './svgs/arrowDown'
 import Cross from './svgs/cross'
 import Hexagon from './svgs/hexagon'
 import DoubleTriangle from './svgs/doubleTriangle'
 import DoubleArrow from './svgs/doubleArrow'
 import Triangle from './svgs/triangle'
+import StaticShapes from './svgs/staticShapes'
 
 const backGroundShapes = () => {
    return (
       <div>
          <Circle />
          <Square />
-         <ArrowUp />
-         <ArrowDown />
          <Cross />
          <Hexagon />
          <DoubleTriangle />
          <DoubleArrow />
          <Triangle />
+         <StaticShapes />
       </div>
    )
 }

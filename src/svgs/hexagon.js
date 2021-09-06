1f09d1b7ef37c7281faf619d3d64585dd2983b3e
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-06 13:56:53 +0100

    --- Auto-Git Commit ---

--- src/svgs/hexagon.js
+++ src/svgs/hexagon.js
@@ -3,7 +3,7 @@ import './shapeStyle.css'
 
 function hexagon() {
    return (
-      <svg width="200" height="250" id={'hexagon'}>
+      <svg id={'hexagon'} viewBox="0 0 30 30">
          <polygon stroke-linejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon>
       </svg>
    )
--- src/svgs/triangle.js
+++ src/svgs/triangle.js
@@ -3,8 +3,8 @@ import './shapeStyle.css'
 
 function triangle() {
    return (
-      <svg id={'triangle'}>
-         <polygon stroke-width="1px" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
+      <svg id={'triangle'} viewBox="0 0 30 30">
+         <polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
       </svg>
    )
 }

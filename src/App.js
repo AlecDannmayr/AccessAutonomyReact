b887aad77a72203b688a76d9a9335f9af2040fe8
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-06 12:56:29 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './style.css'
+import Circle from './svgs/circle'
+import Square from './svgs/square'
+import ArrowUp from './svgs/arrowUp'
+import ArrowDown from './svgs/arrowDown'
+import Cross from './svgs/cross'
+import Hexagon from './svgs/hexagon'
+import Triangle from './svgs/triangle'
 
-function App() {
+const App = () => {
    return (
       <div className="App">
-         <header className="App-header">
-            <svg width="200" height="250">
-               <path d="M14.83,20.82h0a1.28,1.28,0,0,1-1-.52L.25,2a1.27,1.27,0,0,1,2-1.51L14.84,17.45,27.73.5a1.26,1.26,0,0,1,2,1.53L15.84,20.32A1.28,1.28,0,0,1,14.83,20.82Z"></path>
-            </svg>
-         </header>
+         <header className="App-header"></header>
+         <Circle />
+         <Square />
+         <ArrowUp />
+         <ArrowDown />
+         <Cross />
+         <Hexagon />
+         <Triangle />
       </div>
    )
 }

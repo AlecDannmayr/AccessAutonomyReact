7036080d07e59145a22f7cf2fb38d27f622b9796
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-06 13:26:40 +0100

    --- Auto-Git Commit ---

--- src/svgs/triangle.js
+++ src/svgs/triangle.js
@@ -4,7 +4,7 @@ import './shapeStyle.css'
 function triangle() {
    return (
       <svg id={'triangle'}>
-         <polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
+         <polygon stroke-width="1px" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
       </svg>
    )
 }

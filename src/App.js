c51aa3e0c87fcd4da09968fe33193fa085a82302
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-10 18:53:07 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './style.css'
 import BackGroundShapes from './backGroundShapes.js'
+import IntroMDX from './intro.mdx'
 
 const App = () => {
    return (
       <div className="App">
          <layout className="background">
             <BackGroundShapes />
+            <IntroMDX />
          </layout>
+
          <header className="nav"></header>
          <body className="body"></body>
          <footer className="footer"></footer>

0790d163934123c1ca65ef491542d84e2af8f5c3
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-10 21:43:08 +0100

    --- Auto-Git Commit ---

--- src/intro.js
+++ src/intro.js
@@ -5,11 +5,7 @@ const intro = () => {
    return (
       <div className={'intro-div'}>
          <h1 id={'intro-title'}>Access Autonomy</h1>
-         <p id={'intro-body'}>
-            solves complex problems using a process of elimination.
-            <br />
-            There’s never been a more interesting and challenging time to be a developer, the need for virtual communication is more inherent than ever as COVID-19 has changed the way we have previously understood the economic world. More companies than ever are turning to digital communication and remote working to continue to power their business, as developers, it is our job to be problem solvers and figure out how to fill the gaps in communication.
-         </p>
+         <p id={'intro-body'}>solves complex problems using a process of elimination</p>
       </div>
    )
 }

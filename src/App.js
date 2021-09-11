a046da688138cfce6b1efd71487beeaf546d3c0a
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-11 10:16:41 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -6,17 +6,15 @@ import Intro from './intro.js'
 const App = () => {
    const [offsetY, setOffsetY] = useState(0)
    const handleScroll = () => setOffsetY(window.pageYOffset)
-   useEffect =
-      (() => {
-         window.addEventListener('scroll', handleScroll)
-         return () => removeEventListener('scroll', handleScroll)
-      },
-      [])
+   useEffect(() => {
+      window.addEventListener('scroll', handleScroll)
+      return () => window.removeEventListener('scroll', handleScroll)
+   }, [])
    return (
       <div className="App">
          <Intro />
          <layout className="background">
-            <BackGroundShapes />
+            <BackGroundShapes Style={{ transform: `translateY(${offsetY * 0.5}px)` }} />
          </layout>
          <header className="nav"></header>
          <body className="body"></body>

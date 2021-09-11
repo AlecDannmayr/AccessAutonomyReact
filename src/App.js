23cbd2f8b4e1a0a804720ee068040ba0a2c0f06a
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-11 10:02:59 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React, { useState, useEffect } from "react"
 import './style.css'
 import BackGroundShapes from './backGroundShapes.js'
 import Intro from './intro.js'
 
 const App = () => {
+   const [offsetY, setOffsetY] = useState(0);
+   const handleScroll = () => setOffsetY(window.pageYOffset)
+   useEffect = (( => {
+      window.addEventListener('scroll', )
+      return () => removeEventListener("scroll", handleScroll)
+}, []);
    return (
       <div className="App">
          <Intro />

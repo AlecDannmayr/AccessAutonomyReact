ba30ec686a411b4df3e454de26d1a468eea1fbc1
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-12 16:08:58 +0100

    --- Auto-Git Commit ---

--- src/projectBanner.js
+++ src/projectBanner.js
@@ -5,7 +5,6 @@ const projectBanner = () => {
    return (
       <div class="diagonal-box">
          <div class="content">
-            {' '}
             You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition: You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition: You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition: You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this
             effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition:
          </div>

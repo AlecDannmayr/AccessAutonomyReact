94092eb73fae670836f57ab7ffe3dfb955989b55
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-12 16:37:00 +0100

    --- Auto-Git Commit ---

--- src/projectBannerOne.js
+++ src/projectBannerOne.js
@@ -5,7 +5,7 @@ const projectBannerOne = () => {
    return (
       <div class="diagonal-box">
          <div class="content">
-            <p style={'text-align: left;'}>
+            <p>
                You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition: You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition: You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition: You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this
                effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition:
             </p>
--- src/projectBannerTwo.js
+++ src/projectBannerTwo.js
@@ -8,7 +8,7 @@ const projectBannerTwo = () => {
             <figure>
                <img></img>
             </figure>
-            <p style={text-align: right;}>
+            <p>
                You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition: You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition: You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition: You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this
                effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition:
             </p>

1a85ca8f2930d018661c51f6e9cbad8b0e1d691a
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-13 17:45:20 +0100

    --- Auto-Git Commit ---

--- src/copyright.js
+++ src/copyright.js
@@ -3,15 +3,10 @@ import './style.css'
 
 const copyright = () => {
    return (
-      <>
-         <div className={'copyright-div'}>
-            <div class="ocean">
-               <div class="wave"></div>
-               <div class="wave"></div>
-            </div>
-            <p>Copyright© {new Date().getFullYear()} All rights reserved.</p>
-         </div>
-      </>
+      <div class="ocean">
+         <div class="wave"></div>
+         <div class="wave"></div>
+      </div>
    )
 }
 

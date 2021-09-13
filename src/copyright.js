04585486bbdb4bc5e2e24e8b936b94ff64634975
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-13 17:24:43 +0100

    --- Auto-Git Commit ---

--- src/copyright.js
+++ src/copyright.js
@@ -8,7 +8,7 @@ const copyright = () => {
             <p>Copyright© {new Date().getFullYear()} All rights reserved.</p>
             <svg xmlns="http://www.w3.org/2000/svg" id="contact-wave" viewBox="0 0 800 338.05" preserveAspectRatio="none">
                <path>
-                  <animate attributeName="d" values="M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 200 150 400 200 Q 600 250 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z" repeatCount="indefinite" dur="30s"></animate>
+                  <animate attributeName="d" values="M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 200 150 400 200 Q 600 250 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z 100 Z 100 Z 100 Z " repeatCount="indefinite" dur="3s"></animate>
                </path>
             </svg>
          </div>

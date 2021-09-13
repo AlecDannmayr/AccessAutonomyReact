37b70dcc5f7f7d95574bb8b04ba3c61ffe16261b
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-13 17:35:02 +0100

    --- Auto-Git Commit ---

--- src/copyright.js
+++ src/copyright.js
@@ -10,9 +10,7 @@ const copyright = () => {
                   <animate attributeName="d" values="M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 200 150 400 200 Q 600 250 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z; M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z" repeatCount="indefinite" dur="20s"></animate>
                </path>
             </svg>
-         </div>
-         <div>
-            <p id={'copyright-text'}>Copyright© {new Date().getFullYear()} All rights reserved.</p>
+            <p>Copyright© {new Date().getFullYear()} All rights reserved.</p>
          </div>
       </>
    )

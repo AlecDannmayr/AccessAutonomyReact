b664d081d1fc18671c0680229220a4b252c47269
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-13 14:03:30 +0100

    --- Auto-Git Commit ---

---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+const footer = () => {
+   return (
+      <div className={'footer-div'}>
+         <h1 id={'footer-title'}>AccessAutonomy</h1>
+         <p id={'intro-body'}>We solve complex design problems using a finesse for technology.</p>
+      </div>
+   )
+}
+
+export default intro

0e0d82e5f03f162fc384f2906a3ef51d0ee0cf24
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-13 17:00:40 +0100

    --- Auto-Git Commit ---

--- src/footer.js
+++ src/footer.js
@@ -3,10 +3,15 @@ import './style.css'
 
 const footer = () => {
    return (
-      <div className={'footer-div'}>
-         <h1 id={'footer-title'}>Get in touch</h1>
-         <p id={'footer-body'}>Say Hi or find us on other platforms: LinkedIn</p>
-      </div>
+      <>
+         <div className={'footer-div'}>
+            <h1 id={'footer-title'}>Get in touch</h1>
+            <p id={'footer-body'}>Say Hi or find us on other platforms: LinkedIn</p>
+         </div>
+         <div className={'copyright-div'}>
+            <p>Copyright © {new Date().getFullYear()}</p>
+         </div>
+      </>
    )
 }
 

79c1cb8477dd7d1b26fa61102d990e8eb0b27a62
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-14 17:23:42 +0100

    --- Auto-Git Commit ---

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../css/style.css'
+import { Link } from 'react-router-dom'
 
 const footer = () => {
    return (
       <>
          <div className={'footer-div'}>
+            <Link to =''
             <h1 id={'footer-title'}>Get in touch</h1>
             <p id={'footer-body'}>Say Hi, and find us on LinkedIn</p>
          </div>

5acac25e5d6d4c2b9824bfc29235cbdc3d0c0124
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-14 16:06:38 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './css/responsive.css'
 import Intro from './components/intro.js'
 import Footer from './components/footer.js'
 import Copyright from './components/copyright.js'
+import GetInTouch from './pages/getInTouch.js'
+
 import BackGroundShapes from './components/backGroundShapes.js'
 import ProjectBannerOne from './components/projectBannerOne'
 import ProjectBannerTwo from './components/projectBannerTwo'
 import ProjectBannerThree from './components/projectBannerThree'
 import ProjectBannerFour from './components/projectBannerFour'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const App = () => {
    const [offsetY, setOffsetY] = useState(0)
@@ -18,27 +21,34 @@ const App = () => {
       return () => window.removeEventListener('scroll', handleScroll)
    }, [])
    return (
-      <div className="App">
-         <header className="nav"></header>
-         <Intro />
-         <layout className="background" style={{ transform: `translateY(${offsetY * 0.3}px)` }}>
-            <BackGroundShapes />
-         </layout>
-         <section style={{ transform: `translateY(${offsetY * 0.1}px)` }}>
-            <ProjectBannerOne />
-         </section>
-         <section style={{ transform: `translateY(${offsetY * 0}px)` }}>
-            <ProjectBannerFour />
-         </section>
-         <section style={{ transform: `translateY(${offsetY * -0.1}px)` }}>
-            <ProjectBannerTwo />
-         </section>
-         <section style={{ transform: `translateY(${offsetY * -0.2}px)` }}>
-            <ProjectBannerThree />
-         </section>
-         <Footer />
-         <Copyright />
-      </div>
+      <Router>
+         <div className="App">
+            <header className="nav"></header>
+            <Intro />
+            <layout className="background" style={{ transform: `translateY(${offsetY * 0.3}px)` }}>
+               <BackGroundShapes />
+            </layout>
+            <Switch>
+               <Route exact path="/getInTouch">
+                  <GetInTouch />
+               </Route>
+            </Switch>
+            <section style={{ transform: `translateY(${offsetY * 0.1}px)` }}>
+               <ProjectBannerOne />
+            </section>
+            <section style={{ transform: `translateY(${offsetY * 0}px)` }}>
+               <ProjectBannerFour />
+            </section>
+            <section style={{ transform: `translateY(${offsetY * -0.1}px)` }}>
+               <ProjectBannerTwo />
+            </section>
+            <section style={{ transform: `translateY(${offsetY * -0.2}px)` }}>
+               <ProjectBannerThree />
+            </section>
+            <Footer />
+            <Copyright />
+         </div>
+      </Router>
    )
 }
 
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,26 +1,18 @@
 import React from 'react'
 import '../css/style.css'
-import GetInTouch from '../pages/getInTouch.js'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const footer = () => {
    return (
-      <Router>
-         <>
-            <div className={'footer-div'}>
-               <Switch>
-                  <Route exact path="/getInTouch">
-                     <GetInTouch />
-
-                     <h1 id={'footer-title'}>Get in touch</h1>
-                  </Route>
-               </Switch>
-               <p id={'footer-body'}>
-                  Say <a href="_blank">Hi</a> and find us on LinkedIn
-               </p>
-            </div>
-         </>
-      </Router>
+      <>
+         <div className={'footer-div'}>
+            <h1 id={'footer-title'}>
+               <a href="./pages/getInTouch.js">Get in touch</a>
+            </h1>
+            <p id={'footer-body'}>
+               Say <a href="_blank">Hi</a> and find us on LinkedIn
+            </p>
+         </div>
+      </>
    )
 }
 

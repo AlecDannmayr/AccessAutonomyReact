b78929b7ef5c4816c4cdd57664c61cff0c911a49
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-14 15:59:47 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
                <BackGroundShapes />
             </layout>
             <Switch>
-               <Route path="/getInTouch">
+               <Route exact path="/getInTouch">
                   <GetInTouch />
                </Route>
             </Switch>
--- src/components/footer.js
+++ src/components/footer.js
@@ -5,9 +5,7 @@ const footer = () => {
    return (
       <>
          <div className={'footer-div'}>
-            <h1 id={'footer-title'}>
-               <a href="./pages/getInTouch.js">Get in touch</a>
-            </h1>
+            <h1 id={'footer-title'}>Get in touch</h1>
             <p id={'footer-body'}>
                Say <a href="_blank">Hi</a> and find us on LinkedIn
             </p>

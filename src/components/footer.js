d4c261f2f306ab3eddedfe98dceff7c48f3c5288
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-14 17:27:08 +0100

    --- Auto-Git Commit ---

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,8 +6,9 @@ const footer = () => {
    return (
       <>
          <div className={'footer-div'}>
-            <Link to =''
-            <h1 id={'footer-title'}>Get in touch</h1>
+            <Link to="./getintouch">
+               <h1 id={'footer-title'}>Get in touch</h1>
+            </Link>
             <p id={'footer-body'}>Say Hi, and find us on LinkedIn</p>
          </div>
       </>

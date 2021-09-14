f7c6fd25326fa16cdda8b194a9ddab34692eef42
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-14 16:10:05 +0100

    --- Auto-Git Commit ---

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ const footer = () => {
       <>
          <div className={'footer-div'}>
             <h1 id={'footer-title'}>
-               <a href="./pages/getInTouch.js">Get in touch</a>
+               <a href="/getInTouch">Get in touch</a>
             </h1>
             <p id={'footer-body'}>
                Say <a href="_blank">Hi</a> and find us on LinkedIn

9e25ab7a4771e3d4e87363c732e131c8ddf9effb
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-14 17:13:25 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import './css/responsive.css'
 import Intro from './components/intro.js'
 import Footer from './components/footer.js'
 import Copyright from './components/copyright.js'
-import Projects from './components/projects.js'
 import GetInTouch from './pages/getInTouch.js'
 import BackGroundShapes from './components/backGroundShapes.js'
+import ProjectBannerOne from './components/projectBannerOne'
+import ProjectBannerTwo from './components/projectBannerTwo'
+import ProjectBannerThree from './components/projectBannerThree'
+import ProjectBannerFour from './components/projectBannerFour'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 
 const App = () => {
    const [offsetY, setOffsetY] = useState(0)
@@ -19,18 +23,41 @@ const App = () => {
    return (
       <Router>
          <div className="App">
-            <Intro />
             <layout className="background" style={{ transform: `translateY(${offsetY * 0.3}px)` }}>
                <BackGroundShapes />
             </layout>
-            <Projects />
             <Route path="/" component={App} />
             <Route path="/getintouch" component={GetInTouch} />
-            <Footer />
+
             <Copyright />
          </div>
       </Router>
    )
 }
+
+const Home = () => {
+    const [offsetY, setOffsetY] = useState(0)
+   const handleScroll = () => setOffsetY(window.pageYOffset)
+   useEffect(() => {
+      window.addEventListener('scroll', handleScroll)
+      return () => window.removeEventListener('scroll', handleScroll)
+   }, [])
+   <div>
+      <Intro />
+      <section style={{ transform: `translateY(${offsetY * 0.1}px)` }}>
+         <ProjectBannerOne />
+      </section>
+      <section style={{ transform: `translateY(${offsetY * 0}px)` }}>
+         <ProjectBannerFour />
+      </section>
+      <section style={{ transform: `translateY(${offsetY * -0.1}px)` }}>
+         <ProjectBannerTwo />
+      </section>
+      <section style={{ transform: `translateY(${offsetY * -0.2}px)` }}>
+         <ProjectBannerThree />
+      </section>
+      <Footer />
+   </div>
+)
 
 export default App

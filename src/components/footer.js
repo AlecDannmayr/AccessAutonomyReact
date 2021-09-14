f8e5a415a63353e73369ef4909be6d704f632884
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-14 16:44:32 +0100

    --- Auto-Git Commit ---

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,12 +5,8 @@ const footer = () => {
    return (
       <>
          <div className={'footer-div'}>
-            <h1 id={'footer-title'}>
-               <a href="/getInTouch">Get in touch</a>
-            </h1>
-            <p id={'footer-body'}>
-               Say <a href="_blank">Hi</a> and find us on LinkedIn
-            </p>
+            <h1 id={'footer-title'}>Get in touch</h1>
+            <p id={'footer-body'}>Say Hi, and find us on LinkedIn</p>
          </div>
       </>
    )

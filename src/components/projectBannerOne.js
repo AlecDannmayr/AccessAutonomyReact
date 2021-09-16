6376af86fafbb4f75676445a9ca88e6e17c254ef
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-16 22:40:43 +0100

    --- Auto-Git Commit ---

--- src/components/projectBannerOne.js
+++ src/components/projectBannerOne.js
@@ -7,10 +7,7 @@ const projectBannerOne = () => {
    return (
       <div class="diagonal-box-one">
          <div class="content-one">
-            <p style={{ opacity: 1 }}>
-               You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition: You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition: You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition: You may have noticed that now the whole section is transformed, and with it also the content-div living in it. Even though this
-               effect can be quite lovely, sometimes you don't want the content to inherit the given transformation. To reset everything back to normal, you have to reverse the transition:
-            </p>
+           
          </div>
          <iframe src="../../WidgetSubBasic" />
       </div>

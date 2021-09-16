c317788c0ce1347ff97e3b63d31baba754eddaf3
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-16 15:46:17 +0100

    --- Auto-Git Commit ---

--- src/components/projectBannerOne.js
+++ src/components/projectBannerOne.js
@@ -14,7 +14,7 @@ const projectBannerOne = () => {
          </div>
          <figure>
             <div class="frame">
-               <iframe src="../../WidgetSubBasic" />
+               <iframe src="" />
             </div>
          </figure>
       </div>

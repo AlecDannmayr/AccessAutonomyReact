d7bb348d8f9b6763efa014b2d86dd9843fe79995
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-21 17:21:00 +0100

    --- Auto-Git Commit ---

--- src/components/projectBannerOne.js
+++ src/components/projectBannerOne.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import '../css/style.css'
-import projectImgOne from './projectImgOne.js'
 
 const projectBannerOne = () => {
    return (
       <div class="diagonal-box-one">
          <div class="content-one">
-            <iframe title="Basic Scoreboard Iframe" src="%PUBLIC_URL%/WidgetSubBasic" />
+            <iframe title="Basic Scoreboard Iframe" src="../../public/WidgetSubBasic/index.html" />
          </div>
       </div>
    )

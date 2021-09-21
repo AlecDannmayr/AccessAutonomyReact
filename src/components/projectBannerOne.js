ca9eda0fdbf129b43e99dc06a41fe3c203c4cff3
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-21 20:51:55 +0100

    --- Auto-Git Commit ---

--- src/components/projectBannerOne.js
+++ src/components/projectBannerOne.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import '../css/style.css'
 
+const iframe = '<iframe height="265" style="width: 100%;" scrolling="no" title="fx." src="//codepen.io/ycw/embed/JqwbQw/?height=265&theme-id=0&default-tab=js,result" frameborder="no" allowtransparency="true" allowfullscreen="true">See the Pen <a href="https://codepen.io/ycw/pen/JqwbQw/">fx.</a> by ycw(<a href="https://codepen.io/ycw">@ycw</a>) on <a href="https://codepen.io">CodePen</a>.</iframe>'
 const projectBannerOne = () => {
    return (
       <div className="diagonal-box-one">
          <div className="content-one">
             <p style={{ opacity: 1 }}></p>
          </div>
-         <iframe src="../../WidgetSubBasic/index.html" title="Item" />
+         <iframe iframe={iframe} title="Item" />
       </div>
    )
 }

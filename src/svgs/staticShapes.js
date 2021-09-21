8dd1ec795e6c3e873d0a6ad6c05250b60ae004b2
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-21 20:45:05 +0100

    --- Auto-Git Commit ---

--- src/svgs/staticShapes.js
+++ src/svgs/staticShapes.js
@@ -47,37 +47,37 @@ function staticShapes() {
             <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
          </svg>
          <svg id={'static-triangle-1'} viewBox="0 0 30 30">
-            <polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
+            <polygon stroke-width="1px" strokeLinejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
          </svg>
          <svg id={'static-triangle-2'} viewBox="0 0 30 30">
-            <polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
+            <polygon stroke-width="1px" strokeLinejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
          </svg>
          <svg id={'static-triangle-3'} viewBox="0 0 30 30">
-            <polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
+            <polygon stroke-width="1px" strokeLinejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
          </svg>
          <svg id={'static-triangle-4'} viewBox="0 0 30 30">
-            <polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
+            <polygon stroke-width="1px" strokeLinejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
          </svg>
          <svg id={'static-triangle-5'} viewBox="0 0 30 30">
-            <polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
+            <polygon stroke-width="1px" strokeLinejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
          </svg>
          <svg id={'static-triangle-6'} viewBox="0 0 30 30">
-            <polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
+            <polygon stroke-width="1px" strokeLinejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
          </svg>
          <svg id={'static-triangle-7'} viewBox="0 0 30 30">
-            <polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
+            <polygon stroke-width="1px" strokeLinejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
          </svg>
          <svg id={'static-triangle-8'} viewBox="0 0 30 30">
-            <polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
+            <polygon stroke-width="1px" strokeLinejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
          </svg>
          <svg id={'static-triangle-9'} viewBox="0 0 30 30">
-            <polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
+            <polygon stroke-width="1px" strokeLinejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
          </svg>
          <svg id={'static-triangle-10'} viewBox="0 0 30 30">
-            <polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
+            <polygon stroke-width="1px" strokeLinejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
          </svg>
          <svg id={'static-triangle-11'} viewBox="0 0 30 30">
-            <polygon stroke-width="1px" stroke-linejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
+            <polygon stroke-width="1px" strokeLinejoin="round" stroke-miterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
          </svg>
          <svg id={'static-circle-1'} viewBox="0 0 30 30">
             <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
@@ -125,19 +125,19 @@ function staticShapes() {
             <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
          </svg>
          <svg id={'static-hexagon-1'} viewBox="0 0 30 30">
-            <polygon stroke-linejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon>
+            <polygon strokeLinejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon>
          </svg>
          <svg id={'static-hexagon-2'} viewBox="0 0 30 30">
-            <polygon stroke-linejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon>
+            <polygon strokeLinejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon>
          </svg>
          <svg id={'static-hexagon-3'} viewBox="0 0 30 30">
-            <polygon stroke-linejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon>
+            <polygon strokeLinejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon>
          </svg>
          <svg id={'static-hexagon-4'} viewBox="0 0 30 30">
-            <polygon stroke-linejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon>
+            <polygon strokeLinejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon>
          </svg>
          <svg id={'static-hexagon-5'} viewBox="0 0 30 30">
-            <polygon stroke-linejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon>
+            <polygon strokeLinejoin="round" stroke-miterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon>
          </svg>
          <svg id={'static-doubleArrow-1'} viewBox="0 0 30 44.58">
             <path d="M28.74,44.58a1.28,1.28,0,0,1-1-.51L15.16,27.13l-12.89,17a1.26,1.26,0,1,1-2-1.53l13.9-18.29a1.34,1.34,0,0,1,1-.5,1.24,1.24,0,0,1,1,.51L29.75,42.56a1.27,1.27,0,0,1-1,2Z"></path>

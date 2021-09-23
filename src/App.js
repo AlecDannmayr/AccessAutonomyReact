1e3c171ba124e5bdc1ad414bf86498d879878fbc
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-23 10:48:23 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './css/style.css'
 import './css/responsive.css'
 import Intro from './components/intro.js'
 import Footer from './components/footer.js'
-import Copyright from './components/copyright.js'
 import GetInTouch from './pages/getInTouch.js'
 import BackGroundShapes from './components/backGroundShapes.js'
 
@@ -46,8 +45,7 @@ const App = () => {
                <ProjectBannerThree />
             </section>
             */}
-            {/*   <Footer /> */}
-            <Copyright />
+            <Footer />
          </div>
       </Router>
    )

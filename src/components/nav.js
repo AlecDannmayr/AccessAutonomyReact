4058a9d207e868973e47cd815e478def69e5897c
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-23 13:00:35 +0100

    --- Auto-Git Commit ---

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Logo from '../images/Accesss-Autonomy-Logo.ico'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-const element = <FontAwesomeIcon icon={faCoffee} />
 const nav = () => {
    return (
       <div className="nav-main">
@@ -10,7 +9,7 @@ const nav = () => {
             <img src={Logo}></img>
          </div>
          <ul id={'nav-main-list'}>
-            <i src={faCoffee}></i>
+            <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
          </ul>
       </div>
    )

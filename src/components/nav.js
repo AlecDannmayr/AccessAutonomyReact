7abc65669cccd2497607badb7730684889994630
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-09-23 12:40:00 +0100

    --- Auto-Git Commit ---

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Logo from '../images/Accesss-Autonomy-Logo.ico'
 
 const nav = () => {
    return (
       <div className="nav-main">
-         <div id={'nav-main-logo'}></div>
+         <div id={'nav-main-logo'}>
+            <img src={Logo}></img>
+         </div>
          <ul id={'nav-main-list'}>
             <li>
                <i className="fas fa-lg fa-trash-alt"></i>

ddf4f0c9d44c2c6c23198fd8f687c0b48d130c08
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-04 16:26:08 +0100

    --- Auto-Git Commit ---

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouseUser, faAddressCard, faCode, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { Link } from 'react-router-dom'
+
 const nav = () => {
    return (
       <div className="nav-main">

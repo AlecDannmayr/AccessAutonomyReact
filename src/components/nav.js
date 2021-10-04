3cd44a47fac1d7119555dbb94ec509be0ac10536
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-04 16:09:00 +0100

    --- Auto-Git Commit ---

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,6 +3,8 @@ import Logo from '../images/Accesss-Autonomy-Logo.ico'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouseUser, faAddressCard, faCode, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { Link } from 'react-router-dom'
+
 const nav = () => {
    return (
       <div className="nav-main">

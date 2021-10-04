2d9198690f6d3ff6c58b61aff19330ff6870443c
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-04 17:18:08 +0100

    --- Auto-Git Commit ---

--- src/components/about.js
+++ src/components/about.js
@@ -4,9 +4,6 @@ import '../css/style.css'
 const about = () => {
    return (
       <div className="diagonal-box-one">
-         <div className="content-one">
-            <p style={{ opacity: 1 }}></p>
-         </div>
          <iframe src="../../WidgetSubBasic" title="Item" />
       </div>
    )

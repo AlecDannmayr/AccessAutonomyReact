67729d2574769b40d9fb31da8e3536043e20f5cd
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-04 15:17:36 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './css/responsive.css'
 import BackGroundShapes from './components/backGroundShapes.js'
 import Nav from './components/nav.js'
 import Intro from './components/intro.js'
-// import Widget from './components/widget.js'
-//import Footer from './components/footer.js'
+import Widget from './components/widget.js'
+import Footer from './components/footer.js'
 
 const App = () => {
    const [offsetY, setOffsetY] = useState(0)
@@ -19,11 +19,11 @@ const App = () => {
       <div className="App">
          <Nav />
          <Router>
-            <Route path="/Intro" component={} />
-            <BackGroundShapes />
-            {/*<Widget />*/}
+            <Route path="/Intro" component={Intro} />
+            <Route path="/widget" component={Widget} />
             {/*<Footer />*/}
          </Router>
+         <BackGroundShapes />
       </div>
    )
 }

4e31521d8f1b25bb67b9dd1bb143ad029edbc2dd
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-11 22:00:22 +0100

    --- Auto-Git Commit ---

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../css/style.css'
 import '../css/responsive.css'
-import ProjectCards from './projects/projectCards'
+import ProjectImgOne from '../images/image-one.png'
 
 const projects = () => {
    return (
@@ -9,7 +9,22 @@ const projects = () => {
          <h1 id={'project-title'}>
             <div>Projects</div>
          </h1>
-         <ProjectCards />
+         <div class="row">
+            <div class="col s12 m7">
+               <div class="card">
+                  <div class="card-image">
+                     <img src={ProjectImgOne}></img>
+                     <span class="card-title">Card Title</span>
+                  </div>
+                  <div class="card-content">
+                     <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                  </div>
+                  <div class="card-action">
+                     <a href="#">This is a link</a>
+                  </div>
+               </div>
+            </div>
+         </div>
       </div>
    )
 }

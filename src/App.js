117ec8ba279a40a9b79b073f3bac2a62c6e19e41
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-11 18:20:24 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ const App = () => {
    }, [])
    return (
       <div className="App">
+         <News />
          <Router>
             <Nav />
             <Route path="/intro" component={Intro} />

7fc65c872f7507d1662775d0fed623a3b68c3c84
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-11 21:46:37 +0100

    --- Auto-Git Commit ---

--- src/components/projects/projectCards.js
+++ src/components/projects/projectCards.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import '../../css/style.css'
-import ProjectImgOne from '../../images/Accesss-Autonomy-Logo.ico'
+import ProjectImgOne from '../../images/image-one.png'
 
 const projectCards = () => {
    return (
-      <div className="col s2">
+      <div className="col s1">
          <div className="card">
             <div className="card-image">
                <img src={ProjectImgOne} alt={'Access Autonomy Logo'} />

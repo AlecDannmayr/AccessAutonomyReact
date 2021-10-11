46c0df5d4f3d285651922f992fa852e036b7142e
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-11 20:58:35 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import News from './components/news/news'
 const App = () => {
    return (
       <div className="App">
+         <News />
          <Router>
             <Nav />
             <Switch>

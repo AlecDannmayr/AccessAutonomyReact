d9777bb5ccb4b12ce354ce82726283518e292b10
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-11 22:48:31 +0100

    --- Auto-Git Commit ---

--- src/components/projects.js
+++ src/components/projects.js
@@ -34,8 +34,32 @@ const projects = () => {
                   <a href="#">This is a link</a>
                </div>
             </div>
+            <div className="card z-depth-5 transparent">
+               <div className="card-image">
+                  <img src={ProjectImgOne}></img>
+               </div>
+               <div className="card-content">
+                  <span className="card-title">Card Title</span>
+                  <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+               </div>
+               <div className="card-action">
+                  <a href="#">This is a link</a>
+               </div>
+            </div>
          </div>
          <div className="row project-cards">
+            <div className="card z-depth-5 transparent">
+               <div className="card-image">
+                  <img src={ProjectImgOne}></img>
+               </div>
+               <div className="card-content">
+                  <span className="card-title">Card Title</span>
+                  <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+               </div>
+               <div className="card-action">
+                  <a href="#">This is a link</a>
+               </div>
+            </div>
             <div className="card z-depth-5 transparent">
                <div className="card-image">
                   <img src={ProjectImgOne}></img>

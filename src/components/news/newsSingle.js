4d2f01346217f3b58fb384882147e5fad1ee5efd
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-11 20:07:11 +0100

    --- Auto-Git Commit ---

--- src/components/news/newsSingle.js
+++ src/components/news/newsSingle.js
@@ -2,15 +2,15 @@ import React from 'react'
 import '../../css/style.css'
 
 const NewsSingle = ({ item }) => (
-   <div className="card-div">
-      <div className="car">
+   <div className="col s1 card-div">
+      <div className="card">
          <div className="card-image">
             <img src={item.urlToImage} alt={item.title} />
          </div>
-         <div className="">
+         <div className="card-content">
             <p ref={this.cardText}>{item.title}</p>
          </div>
-         <div className="">
+         <div className="card-action">
             <a href={item.url} target="_blank">
                Full article
             </a>

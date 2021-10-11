54544ae8a2665f1a53e092c480d3a402b3c060b4
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-11 21:50:05 +0100

    --- Auto-Git Commit ---

--- src/components/projects/projectCards.js
+++ src/components/projects/projectCards.js
@@ -4,26 +4,24 @@ import ProjectImgOne from '../../images/image-one.png'
 
 const projectCards = () => {
    return (
-      <div className="col s1">
-         <div className="card">
-            <div className="card-image">
-               <img src={ProjectImgOne} alt={'Access Autonomy Logo'} />
-            </div>
-            <div className="card-content">
-               <p></p>
-            </div>
-            <div className="card-action"></div>
-         </div>
-         <div className="card">
-            <div className="card-image">
-               <img src={ProjectImgOne} alt={'Access Autonomy Logo'} />
-            </div>
-            <div className="card-content">
-               <p></p>
-            </div>
-            <div className="card-action"></div>
-         </div>
+
+  <div class="row">
+    <div class="col s12 m7">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/sample-1.jpg">
+          <span class="card-title">Card Title</span>
+        </div>
+        <div class="card-content">
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+        </div>
       </div>
+    </div>
+          
    )
 }
 

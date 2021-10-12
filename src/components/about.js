123d1fc7a2535863d9cc5e1b05c51bd48bbde949
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-12 22:08:55 +0100

    --- Auto-Git Commit ---

--- src/components/about.js
+++ src/components/about.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import '../css/style.css'
 import Development from '../images/development.svg'
+import Design from '../images/design.svg'
+import Maintain from '../images/maintain.svg'
+import Teach from '../images/teach.svg'
+import Consult from '../images/consult.svg'
 
 const about = () => {
-   return
-    <img src={Development}></img>
-       <img src={Design}></img>
-          <img src={Maintain}></img>
+   return (
+      <div className="services">
+         <img src={Development}></img>
+         <img src={Design}></img>
+         <img src={Maintain}></img>
+         <img src={Teach}></img>
+         <img src={Consult}></img>
+      </div>
+   )
 }
 
 export default about

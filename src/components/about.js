38e68bbd8b99f0f5b6e46a1a3268a1c69c35297a
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-12 22:26:08 +0100

    --- Auto-Git Commit ---

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../css/style.css'
-import Development from '../images/development.svg'
+import Development from '../images/develop.svg'
 import Design from '../images/design.svg'
 import Maintain from '../images/maintain.svg'
 import Teach from '../images/teach.svg'

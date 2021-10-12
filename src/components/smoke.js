cbe03f5c3e8d0e4b2b671e0cc0579040ae1a20a6
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-12 22:46:55 +0100

    --- Auto-Git Commit ---

--- src/components/smoke.js
+++ src/components/smoke.js
@@ -1,18 +1,34 @@
 import React from 'react'
 import '../css/style.css'
 import Video from '../video/smoke.mp4'
-
+import Logo from '../images/accessautonomy.svg'
 
 const smoke = () => {
    return (
-   <section class="open">
-		<div class="open-animation">
-			<video src={Video} autoplay muted></video>
-			<h1><span>A</span><span>C</span><span>C</span><span>E</span><span>S</span><span>S</span><span>A</span><span>U</span><span>T</span><span>O</span><span>N</span><span>O</span><span>M</span><span>Y</span>
-			</h1>
-			<figure class="logo-animate"><img src="images/accessautonomy.svg" alt="Access Autonomy Logo"></figure>
-		</div>
-	</section>
+      <section class="open">
+         <div class="open-animation">
+            <video src={Video} autoplay muted></video>
+            <h1>
+               <span>A</span>
+               <span>C</span>
+               <span>C</span>
+               <span>E</span>
+               <span>S</span>
+               <span>S</span>
+               <span>A</span>
+               <span>U</span>
+               <span>T</span>
+               <span>O</span>
+               <span>N</span>
+               <span>O</span>
+               <span>M</span>
+               <span>Y</span>
+            </h1>
+            <figure class="logo-animate">
+               <img src={Logo} alt="Access Autonomy Logo"></img>
+            </figure>
+         </div>
+      </section>
    )
 }
 

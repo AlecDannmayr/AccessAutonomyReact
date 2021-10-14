40b831158a586e7e84812c9ad4a1478ca93fe3b5
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-14 13:39:13 +0100

    --- Auto-Git Commit ---

--- src/theme.js
+++ src/theme.js
@@ -1,1 +1,16 @@
-import { createMuiTheme } from '@material-ui/core/styles'
+import { createTheme } from '@mui/material/styles'
+import { green, purple } from '@mui/material/colors'
+
+const theme = createTheme({
+   palette: {
+      primary: {
+         main: purple[500],
+      },
+      secondary: {
+         main: green[500],
+      },
+   },
+   background: {
+      default: '#ffffff',
+   },
+})

e7e9ddfd41fe79fc33c369501dd41575c63e54a7
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-14 15:18:44 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-
 import './css/style.css'
 import './css/responsive.css'
 import BackGroundShapes from './components/backGroundShapes.js'

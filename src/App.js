63960ccb8d7bc15f173de50472212235f61292fe
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-14 16:27:34 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -34,6 +34,16 @@ const themes = {
    dark: DarkTheme,
 }
 
+function Splash(props) {
+   function changeTime() {
+      if (props.theme === 'light') {
+         props.setTheme('dark')
+      } else {
+         props.setTheme('light')
+      }
+   }
+}
+
 const icon = props.theme === 'light' ? <HiMoon size={40} /> : <CgSun size={40} />
 
 const App = () => {

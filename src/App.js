b1acde52932e203b44115554eaeeca58310a919b
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-14 17:40:10 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -37,12 +37,10 @@ const App = () => {
    return (
       <div className="App">
          <ThemeProvider theme={themes[theme]}>
-            <Splash theme={theme} setTheme={setTheme} />
-
             <Router>
                <Nav />
                <Switch>
-                  <Route path="/" exact component={Intro} />
+                  <Route theme={theme} setTheme={setTheme} path="/" exact component={Intro} />
                   <Route path="/about" component={About} />
                   <Route path="/projects" component={Projects} />
                   <Route path="/blog" component={Blog} />

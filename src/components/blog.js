e4a101591b67bc774bc4b9d56d6672f5dc96c361
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-15 15:58:54 +0100

    --- Auto-Git Commit ---

--- src/components/blog.js
+++ src/components/blog.js
@@ -6,6 +6,11 @@ import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Button from '@mui/material/Button'
 
 const useStyles = makeStyles((theme) => ({
    blogsContainer: {
@@ -36,13 +41,29 @@ const blog = () => {
       <div>
          <Container maxWidth="lg" className={classes.blogsContainer}>
             <Box className={classes.hero}>
-               <Box>React Blog</Box>
+               <Box>Blog</Box>
             </Box>
             <Typography variant="h4" className={classes.blogTitle}>
                Articles
             </Typography>
             <Grid container spacing={3}>
-               <Grid item xs={12} sm={6} md={4} container spacing={3}></Grid>
+               <Grid item xs={12} sm={6} md={4} container spacing={3}>
+                  <Card sx={{ maxWidth: 345 }}>
+                     <CardMedia component="img" height="140" image="/static/images/cards/contemplative-reptile.jpg" alt="green iguana" />
+                     <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                           Lizard
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                           Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
+                        </Typography>
+                     </CardContent>
+                     <CardActions>
+                        <Button size="small">Share</Button>
+                        <Button size="small">Learn More</Button>
+                     </CardActions>
+                  </Card>
+               </Grid>
                <Grid item xs={12} sm={6} md={4} container spacing={3}></Grid>
                <Grid item xs={12} sm={6} md={4} container spacing={3}></Grid>
                <Grid item xs={12} sm={6} md={4} container spacing={3}></Grid>

fa8e40c962be530e92f197e2cf95283601d28c9d
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-15 15:48:35 +0100

    --- Auto-Git Commit ---

--- src/components/blog.js
+++ src/components/blog.js
@@ -3,7 +3,7 @@ import '../css/style.css'
 import '../css/responsive.css'
 import { makeStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/box'
+import Box from '@material-ui/core/Box'
 
 const useStyles = makeStyles((theme) => ({
    blogsContainer: {

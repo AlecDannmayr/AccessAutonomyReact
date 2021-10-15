5f16791dcd34c3853d845e864123e15ae2c1d513
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-15 15:14:15 +0100

    --- Auto-Git Commit ---

--- src/components/blog.js
+++ src/components/blog.js
@@ -6,7 +6,15 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 
+const useStyles = makeStyles((theme) => ({
+   blogsContainer: {
+      paddinTop: theme.spacing(3),
+   },
+}))
+
 const blog = () => {
-   return <div></div>
+   return <div maxWidth="lg" className={classes.blogsContainer}>
+      <Typography
+   </div>
 }
 export default blog

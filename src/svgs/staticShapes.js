f9986a619b655ec27f76cd98f7df17ff26a94691
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-18 15:03:36 +0100

    --- Auto-Git Commit ---

--- src/svgs/staticShapes.js
+++ src/svgs/staticShapes.js
@@ -13,24 +13,7 @@ function staticShapes() {
          <svg id={'static-square-3'} viewBox="0 0 30 30">
             <path d="M28,2V28H2V2H28m.13-2H1.88A1.88,1.88,0,0,0,0,1.88V28.13A1.88,1.88,0,0,0,1.88,30H28.13A1.87,1.87,0,0,0,30,28.13V1.88A1.88,1.88,0,0,0,28.13,0Z"></path>
          </svg>
-         <svg id={'static-square-4'} viewBox="0 0 30 30">
-            <path d="M28,2V28H2V2H28m.13-2H1.88A1.88,1.88,0,0,0,0,1.88V28.13A1.88,1.88,0,0,0,1.88,30H28.13A1.87,1.87,0,0,0,30,28.13V1.88A1.88,1.88,0,0,0,28.13,0Z"></path>
-         </svg>
-         <svg id={'static-square-5'} viewBox="0 0 30 30">
-            <path d="M28,2V28H2V2H28m.13-2H1.88A1.88,1.88,0,0,0,0,1.88V28.13A1.88,1.88,0,0,0,1.88,30H28.13A1.87,1.87,0,0,0,30,28.13V1.88A1.88,1.88,0,0,0,28.13,0Z"></path>
-         </svg>
-         <svg id={'static-square-6'} viewBox="0 0 30 30">
-            <path d="M28,2V28H2V2H28m.13-2H1.88A1.88,1.88,0,0,0,0,1.88V28.13A1.88,1.88,0,0,0,1.88,30H28.13A1.87,1.87,0,0,0,30,28.13V1.88A1.88,1.88,0,0,0,28.13,0Z"></path>
-         </svg>
-         <svg id={'static-square-7'} viewBox="0 0 30 30">
-            <path d="M28,2V28H2V2H28m.13-2H1.88A1.88,1.88,0,0,0,0,1.88V28.13A1.88,1.88,0,0,0,1.88,30H28.13A1.87,1.87,0,0,0,30,28.13V1.88A1.88,1.88,0,0,0,28.13,0Z"></path>
-         </svg>
-         <svg id={'static-square-8'} viewBox="0 0 30 30">
-            <path d="M28,2V28H2V2H28m.13-2H1.88A1.88,1.88,0,0,0,0,1.88V28.13A1.88,1.88,0,0,0,1.88,30H28.13A1.87,1.87,0,0,0,30,28.13V1.88A1.88,1.88,0,0,0,28.13,0Z"></path>
-         </svg>
-         <svg id={'static-square-9'} viewBox="0 0 30 30">
-            <path d="M28,2V28H2V2H28m.13-2H1.88A1.88,1.88,0,0,0,0,1.88V28.13A1.88,1.88,0,0,0,1.88,30H28.13A1.87,1.87,0,0,0,30,28.13V1.88A1.88,1.88,0,0,0,28.13,0Z"></path>
-         </svg>
+
          <svg id={'static-circle-1'} viewBox="0 0 30 30">
             <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
          </svg>
@@ -52,79 +35,10 @@ function staticShapes() {
          <svg id={'static-triangle-2'} viewBox="0 0 30 30">
             <polygon strokeWidth="1px" strokeLinejoin="round" strokeMiterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
          </svg>
-         <svg id={'static-triangle-3'} viewBox="0 0 30 30">
-            <polygon strokeWidth="1px" strokeLinejoin="round" strokeMiterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
-         </svg>
-         <svg id={'static-triangle-4'} viewBox="0 0 30 30">
-            <polygon strokeWidth="1px" strokeLinejoin="round" strokeMiterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
-         </svg>
-         <svg id={'static-triangle-5'} viewBox="0 0 30 30">
-            <polygon strokeWidth="1px" strokeLinejoin="round" strokeMiterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
-         </svg>
-         <svg id={'static-triangle-6'} viewBox="0 0 30 30">
-            <polygon strokeWidth="1px" strokeLinejoin="round" strokeMiterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
-         </svg>
-         <svg id={'static-triangle-7'} viewBox="0 0 30 30">
-            <polygon strokeWidth="1px" strokeLinejoin="round" strokeMiterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
-         </svg>
-         <svg id={'static-triangle-8'} viewBox="0 0 30 30">
-            <polygon strokeWidth="1px" strokeLinejoin="round" strokeMiterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
-         </svg>
-         <svg id={'static-triangle-9'} viewBox="0 0 30 30">
-            <polygon strokeWidth="1px" strokeLinejoin="round" strokeMiterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
-         </svg>
-         <svg id={'static-triangle-10'} viewBox="0 0 30 30">
-            <polygon strokeWidth="1px" strokeLinejoin="round" strokeMiterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
-         </svg>
+
          <svg id={'static-triangle-11'} viewBox="0 0 30 30">
             <polygon strokeWidth="1px" strokeLinejoin="round" strokeMiterlimit="10" points="14.921,2.27 28.667,25.5 1.175,25.5 "></polygon>
          </svg>
-         <svg id={'static-circle-1'} viewBox="0 0 30 30">
-            <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
-         </svg>
-         <svg id={'static-circle-2'} viewBox="0 0 30 30">
-            <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
-         </svg>
-         <svg id={'static-circle-3'} viewBox="0 0 30 30">
-            <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
-         </svg>
-         <svg id={'static-circle-4'} viewBox="0 0 30 30">
-            <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
-         </svg>
-         <svg id={'static-circle-5'} viewBox="0 0 30 30">
-            <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
-         </svg>
-         <svg id={'static-circle-6'} viewBox="0 0 30 30">
-            <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
-         </svg>
-         <svg id={'static-circle-7'} viewBox="0 0 30 30">
-            <path d="M15,30A15,150,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
-         </svg>
-         <svg id={'static-circle-8'} viewBox="0 0 30 30">
-            <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
-         </svg>
-         <svg id={'static-circle-9'} viewBox="0 0 30 30">
-            <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
-         </svg>
-         <svg id={'static-circle-10'} viewBox="0 0 30 30">
-            <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
-         </svg>
-         <svg id={'static-circle-11'} viewBox="0 0 30 30">
-            <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
-         </svg>
-         <svg id={'static-circle-12'} viewBox="0 0 30 30">
-            <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
-         </svg>
-         <svg id={'static-circle-13'} viewBox="0 0 30 30">
-            <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
-         </svg>
-         <svg id={'static-circle-14'} viewBox="0 0 30 30">
-            <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
-         </svg>
-         <svg id={'static-circle-15'} viewBox="0 0 30 30">
-            <path d="M15,30A15,15,0,1,1,30,15,15,15,0,0,1,15,30ZM15,6.23A8.77,8.77,0,1,0,23.77,15,8.77,8.77,0,0,0,15,6.23Z"></path>
-         </svg>
-
          <svg id={'static-hexagon-5'} viewBox="0 0 30 30">
             <polygon strokeLinejoin="round" strokeMiterlimit="10" points="27.5,21.904 15,28.809  2.5,21.904 2.5,8.095 15,1.19 27.5,8.095 "></polygon>
          </svg>

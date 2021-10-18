10736ee67cd240385bd1cd1e94f8c387f0aabfdd
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-18 14:21:37 +0100

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -12,8 +12,28 @@ import Contacts from './components/contacts.js'
 import Copyright from './components/copyright.js'
 import Smoke from './components/smoke'
 import News from './components/news/news'
+import Splash from './components/SplashScreen'
+
+const LightTheme = {
+   pageBackground: 'white',
+   titleColor: '#dc658b',
+   tagLineColor: 'black',
+}
+
+const DarkTheme = {
+   pageBackground: 'white',
+   titleColor: 'lightpink',
+   tagLineColor: 'lavender',
+}
+
+const themes = {
+   light: LightTheme,
+   dark: DarkTheme,
+}
 
 function App() {
+   const [theme, setTheme] = useState('light')
+
    return (
       <div className="App">
          <Router>
---
+++ src/components/SplashScreen.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import styled from 'styled-components'
+import { CgSun } from 'react-icons/cg'
+import { HiMoon } from 'react-icons/hi'
+
+const Toggle = styled.button`
+   background-color: ${(props) => props.theme.titleColor};
+`
+
+const Page = styled.div`
+   background-color: ${(props) => props.theme.titleColor};
+`
+const Container = styled.div`
+   dipslay: flex;
+   flex-direction: column;
+   align-items: center;
+`
+
+const Title = styled.h1`
+   color: ${(props) => props.theme.titleColor};
+`
+
+const TagLine = styled.span`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+`
+
+function Splash(props) {
+   function changeTheme() {
+      if (props.theme === 'light') {
+         props.setTheme('dark')
+      } else {
+         props.setTheme('light')
+      }
+   }
+
+   const icon = props.theme === 'light' ? <HiMoon size={40} /> : <CgSun size={40} />
+
+   return (
+      <Page>
+         <Container>
+            <Toggle onClick={changeTheme}>{icon}</Toggle>
+            <Title>Coding with Chaim</Title>
+            <TagLine>Level up your web development skills</TagLine>
+         </Container>
+      </Page>
+   )
+}
+
+export default Splash

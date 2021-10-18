6fa1f5e67c74a82bda6b2e6fe04ad5fb73d2f89d
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-18 22:22:41 +0100

    --- Auto-Git Commit ---

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -11,29 +11,6 @@ const contacts = () => {
          <p id={''}>Say Hi, and find us on LinkedIn</p>
          <div className="contact-type">
             <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=e20%201as&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-           </div>
-         <div class="class-form">
-      <form action="submit-form" id="submit-form">
-        <label for="fName" class="firstName">First Name</label><br>
-        <input id="firstName" type="text" id="fName" name="firstName"></input>
-        <label for="sName" class="secondName">Second Name</label><br>
-        <input type="text" id="sName" name="secondName"></input>
-
-        <label for="labelCountries">Countries</label>
-        <select id="country" name="country">
-          <option value="">- select -</option>
-          <option value="Australia">Australia</option>
-          <option value="Canada">Canada</option>
-          <option value="Singapore">Singapore</option>
-          <option value="Hong Kong">Hong Kong</option>
-          <option value="New Zealand">New Zealand</option>
-        </select>
-
-
-        <label for="submit" class="submit">Submit</label>
-        <input type="submit" id="submit" name="submit" value="Submit"></input>
-      </form>
- 
          </div>
       </div>
    )

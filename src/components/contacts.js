e9b9d23aafb4dbadcbdaea23db86e2292fe26502
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-18 17:10:46 +0100

    --- Auto-Git Commit ---

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -9,7 +9,10 @@ const contacts = () => {
             <h1 id={''}>Get in touch</h1>
          </Link>
          <p id={''}>Say Hi, and find us on LinkedIn</p>
-      </div>
+         <div className="split">
+            <div></div>
+<div class="mapouter"><div class="gmap_canvas"><iframe width="718" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=20%20liberty%20bridge%20road%20&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.whatismyip-address.com"></a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:718px;}</style><a href="https://www.embedgooglemap.net"></a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:718px;}</style></div></div>
+   
    )
 }
 

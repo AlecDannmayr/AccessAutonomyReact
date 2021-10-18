648f13a33b3c9c0eb367cca8ae97739f22d0905b
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-18 17:14:11 +0100

    --- Auto-Git Commit ---

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../css/style.css'
 import { Link } from 'react-router-dom'
 
+
 const contacts = () => {
    return (
       <div className={'footer'}>
@@ -10,9 +11,33 @@ const contacts = () => {
          </Link>
          <p id={''}>Say Hi, and find us on LinkedIn</p>
          <div className="split">
-            <div></div>
-<div class="mapouter"><div class="gmap_canvas"><iframe width="718" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=20%20liberty%20bridge%20road%20&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.whatismyip-address.com"></a><br><style>.mapouter{position:relative;text-align:right;height:500px;width:718px;}</style><a href="https://www.embedgooglemap.net"></a><style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:718px;}</style></div></div>
-   
+             <div class="class-form">
+      <form action="submit-form" id="submit-form">
+        <label for="fName" class="firstName">First Name</label><br/>
+        <input id="firstName" type="text" id="fName" name="firstName">
+        <label for="sName" class="secondName">Second Name</label><br/>
+        <input type="text" id="sName" name="secondName">
+
+        <label for="labelCountries">Countries</label>
+        <select id="country" name="country">
+          <option value="">- select -</option>
+          <option value="Australia">Australia</option>
+          <option value="Canada">Canada</option>
+          <option value="Singapore">Singapore</option>
+          <option value="Hong Kong">Hong Kong</option>
+          <option value="New Zealand">New Zealand</option>
+        </select>
+
+
+        <label for="submit" class="submit">Submit</label>
+        <input type="submit" id="submit" name="submit" value="Submit">
+      </form>
+    </div>
+            <div>
+               <iframe width="500" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=20%20liberty%20bridge%20road%20&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            </div>
+         </div>
+      </div>
    )
 }
 

443a6c3944faca226eff9e29b8e4915330af44ff
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-18 22:50:43 +0100

    --- Auto-Git Commit ---

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -13,36 +13,41 @@ const contacts = () => {
             <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=e20%201as&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
             <div class="class-form">
                <form action="submit-form" id="submit-form">
-                  <label for="fName" class="firstName">
-                     First Name
-                  </label>
-                  <br />
-                  <input id="firstName" type="text" id="fName" name="firstName"></input>
-                  <label for="sName" class="secondName">
-                     Second Name
-                  </label>
-                  <br />
-                  <input type="text" id="sName" name="secondName"></input>
-                  <label for="company" class="company">
-                     Ccmpany
-                  </label>
-                  <br />
-                  <input id="company" type="text" id="company" name="company"></input>
-                  <label for="email" class="email">
-                     Contact Number
-                  </label>
-                  <br />
-                  <input id="email" type="text" id="email" name="email"></input>
-                  <label for="email" class="email">
-                     Email
-                  </label>
-                  <br />
-                  <input id="email" type="text" id="email" name="email"></input>
+                  <div>
+                     <label for="fName" class="firstName">
+                        First Name
+                     </label>
+                     <br />
+                     <input id="firstName" type="text" id="fName" name="firstName"></input>
+                     <label for="sName" class="secondName">
+                        Second Name
+                     </label>
+                     <br />
+                     <input type="text" id="sName" name="secondName"></input>
+                     <label for="company" class="company">
+                        Company
+                     </label>
+                     <br />
+                     <textarea></textarea>
+                  </div>
+                  <div>
+                     <input id="company" type="text" id="company" name="company"></input>
+                     <label for="email" class="email">
+                        Contact Number
+                     </label>
+                     <br />
+                     <input id="email" type="text" id="email" name="email"></input>
+                     <label for="email" class="email">
+                        Email
+                     </label>
+                     <br />
+                     <input id="email" type="text" id="email" name="email"></input>
 
-                  <label for="submit" class="submit">
-                     Submit
-                  </label>
-                  <input type="submit" id="submit" name="submit" value="Submit"></input>
+                     <label for="submit" class="submit">
+                        Submit
+                     </label>
+                     <input type="submit" id="submit" name="submit" value="Submit"></input>
+                  </div>
                </form>
             </div>
          </div>

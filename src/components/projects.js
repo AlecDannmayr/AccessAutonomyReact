cf08e99bb3205e44586101a4b96fc156f918e8e9
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-18 16:39:53 +0100

    --- Auto-Git Commit ---

--- src/components/projects.js
+++ src/components/projects.js
@@ -21,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
 export default function StandardImageList() {
    const classes = useStyles()
    return (
-      <ImageList lg={{ width: 500 }} cols={3} rowHeight={364} className={classes.imageContainer}>
+      <ImageList lg={{ width: 500 }} cols={3} rowHeight={64} className={classes.imageContainer}>
          {itemData.map((item) => (
             <ImageListItem key={item.img}>
                <img src={`${item.img}?w=164&h=164&fit=crop&auto=format`} srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`} alt={item.title} loading="lazy" />

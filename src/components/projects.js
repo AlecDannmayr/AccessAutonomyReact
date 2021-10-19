24a4688c2fb899003e9a5e6db532cae979740069
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-19 15:50:35 +0100

    --- Auto-Git Commit ---

--- src/components/projects.js
+++ src/components/projects.js
@@ -80,18 +80,19 @@ export default function StandardImageList() {
             <TagButton name="all" handleSetTag={setTag} />
             <TagButton name="Build" handleSetTag={setTag} />
             <TagButton name="UX/UI" handleSetTag={setTag} />
-         </div>*/}
-         <SRLWrapper>
-            <ImageList cols={3} className={classes.imageContainer}>
-               {filteredImages.map((item) => (
-                  <ImageListItem key={item.img}>
-                     <a href={`/images/${item.img}`}></a>
-                     <img id="imageContainer" src={`${item.img}?w=164&h=164&fit=crop&auto=format`} srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`} alt={item.title} loading="lazy" />
-                     <p id="show">{item.title}</p>
-                  </ImageListItem>
-               ))}
-            </ImageList>
-         </SRLWrapper>
+         </div>
+         <SRLWrapper>*/}
+         <ImageList cols={3} className={classes.imageContainer}>
+            {filteredImages.map((item) => (
+               <ImageListItem key={item.img}>
+                  {/*
+                  <a href={`/images/${item.img}`}></a>*/}
+                  <img id="imageContainer" src={`${item.img}?w=164&h=164&fit=crop&auto=format`} srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`} alt={item.title} loading="lazy" />
+                  <p id="show">{item.title}</p>
+               </ImageListItem>
+            ))}
+         </ImageList>
+         {/*</SRLWrapper>*/}
       </div>
    )
 }

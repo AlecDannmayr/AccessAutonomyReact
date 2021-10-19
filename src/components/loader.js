eab3cf4ac15a366ef3e94221efaacb11eccce9a7
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-19 11:18:29 +0100

    --- Auto-Git Commit ---

--- src/components/loader.js
+++ src/components/loader.js
@@ -2,7 +2,11 @@ import React from 'react'
 import '../css/loader.css'
 
 const Loader = () => {
-   return <div className="spinonediv-1"></div>
+   return (
+      <div className="loader-container">
+         <div className="spinonediv-1"></div>
+      </div>
+   )
 }
 
 export default Loader

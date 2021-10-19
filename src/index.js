604676d1dc375ecae0fd16a98d2f0a137ef32718
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-10-19 15:33:16 +0100

    --- Auto-Git Commit ---

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './css/style.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import SimpleReactLightbox from 'simple-react-lightbox'
 
 ReactDOM.render(
    <React.StrictMode>

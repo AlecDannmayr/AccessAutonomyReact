2e341f0ee147de08438c430b41e11ae7dd858ae9
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-11-04 11:36:24 +0000

    --- Auto-Git Commit ---

--- src/components/about.js
+++ src/components/about.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import '../css/style.css'
 import Development from '../images/develop.svg'
 import Design from '../images/design.svg'
@@ -6,23 +6,16 @@ import Maintain from '../images/maintain.svg'
 import Teach from '../images/teach.svg'
 import Consult from '../images/consult.svg'
 
-function about() {
-   const [offsetY, setOffsetY] = useState(0)
-   const handleScroll = () => setOffsetY(window.pageYOffset)
-   useEffect(() => {
-      window.addEventListener('scroll')
-   }, [])
-
-   const renderContent = () => {
-      return (
-         <div className="services">
-            <img src={Development}></img>
-            <img src={Design}></img>
-            <img src={Maintain}></img>
-            <img src={Teach}></img>
-            <img src={Consult}></img>
-         </div>
-      )
-   }
+const about = () => {
+   return (
+      <div className="services">
+         <img src={Development}></img>
+         <img src={Design}></img>
+         <img src={Maintain}></img>
+         <img src={Teach}></img>
+         <img src={Consult}></img>
+      </div>
+   )
 }
+
 export default about

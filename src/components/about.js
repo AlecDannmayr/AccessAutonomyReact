930c55bc9ac2b0c39a6bec450826335f65358a06
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-11-05 21:13:35 +0000

    --- Auto-Git Commit ---

--- src/components/about.js
+++ src/components/about.js
@@ -19,7 +19,10 @@ function About() {
       <>
          <div>
             <div className="services" style={{ transform: `translateY(${offsetY * 0.1}px)` }}>
-               <img src={Consult}></img>
+               <div>
+                  <img src={Consult}></img>
+                  <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+               </div>
                <img src={Design}></img>
                <img src={Development}></img>
                <img src={Teach}></img>

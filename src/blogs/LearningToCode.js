06183468756d3ce20a7e1a1a2b14be1941bca9d3
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-11-10 14:03:39 +0000

    --- Auto-Git Commit ---

--- src/blogs/LearningToCode.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-webpack-loader-syntax */
-import Content from '!babel-loader!@mdx-js/loader!./Content.mdx'
-
-function LearningToCode() {
-   return <Content />
-}
-
-export default LearningToCode

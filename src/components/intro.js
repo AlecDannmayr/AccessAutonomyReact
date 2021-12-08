90b60c3627789c340a0242f73f962007e44958b0
Alec Dannmayr <alec.dannmayr@gmail.com>
2021-12-08 21:43:29 +0000

    --- Auto-Git Commit ---

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,16 +1,31 @@
+/** @format */
+
 import React from 'react'
 import '../css/style.css'
 import '../css/responsive.css'
 
-const intro = () => {
-   return (
-      <div className={'intro-div'}>
-         <div>
-            <h1 id={'intro-title'}>Access Autonomy</h1>
-            <p id={'intro-body'}>Solving complex design problems using a finesse for technology.</p>
-         </div>
-      </div>
-   )
+function SecretComponent() {
+    return <h1>Secret Information Component</h1>
+}
+
+function RegularComponent() {
+    return <h1>Everyone can see this component</h1>
+}
+
+const intro = (props) => {
+    console.log(props)
+    if (props.authorised) {
+        return <SecretComponent />
+    } else {
+        return (
+            <div className={'intro-div'}>
+                <div>
+                    <h1 id={'intro-title'}>Access Autonomy</h1>
+                    <p id={'intro-body'}>Solving complex design problems using a finesse for technology.</p>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default intro
